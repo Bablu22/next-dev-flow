248d2d5dae3cf7e9126f3f7f1fb7e8d060b6a271
Bablu22 <mdbablu22.bablu22@gmail.com>
2024-04-03 10:38:55 +0600

    design right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import RightSidebar from "@/components/shared/RightSidebar";
 import React, { PropsWithChildren } from "react";
 
 const RootLayout = ({ children }: PropsWithChildren) => {
@@ -11,7 +12,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        <div>Right</div>
+        <RightSidebar />
       </div>
       <div>Toaster</div>
     </main>
